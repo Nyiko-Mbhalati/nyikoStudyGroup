ca1b0f7fc18cf3dda5507c9d7bd9113ab3b71930
Nyiko Mbhalati <u24722490@up.ac.za>
2025-02-27 11:10:52 +0200

    Added the student controller. Added the student model.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace nyikoStudyGroup.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult ListStudents()
+        {
+            List<Models.StudentModel> students = new List<Models.StudentModel>();
+
+            students.Add(new Models.StudentModel { _studentNumber = "", _name = "", _surname = "", _emailAddress = ""});
+            students.Add(new Models.StudentModel { _studentNumber = "", _name = "", _surname = "", _emailAddress = "" });
+            students.Add(new Models.StudentModel { _studentNumber = "", _name = "", _surname = "", _emailAddress = "" });
+            students.Add(new Models.StudentModel { _studentNumber = "", _name = "", _surname = "", _emailAddress = "" });
+            students.Add(new Models.StudentModel { _studentNumber = "", _name = "", _surname = "", _emailAddress = "" });
+
+            return View();
+        }
+    }
+}
---
+++ Models/StudentModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace nyikoStudyGroup.Models
+{
+	public class StudentModel
+	{
+		[Display(Name = "Student Number")]
+		public string _studentNumber { get; set; }
+
+		[Display(Name = "Name")]
+		public string _name { get; set; }
+
+        [Display(Name = "Surname")]
+        public string _surname { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string _emailAddress { get; set; }
+    }
+}
