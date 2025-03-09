f0be374fa55343e95ef4ef59cf58855af649c04e
Nyiko Mbhalati <u24722490@tuks.co.za>
2025-03-09 13:00:59 +0200

    Added values for the myLink property that reference the newly created html file of that person in the HTML folder through a link.

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -13,11 +13,11 @@ namespace nyikoStudyGroup.Controllers
         {
             List<Models.StudentModel> students = new List<Models.StudentModel>();
 
-            students.Add(new Models.StudentModel { _studentNumber = "u24722490", _name = "Nyiko", _surname = "Mbhalati", _emailAddress = "u24722490@tuks.co.za"});
-            students.Add(new Models.StudentModel { _studentNumber = "u24641342", _name = "Michael", _surname = "Neto", _emailAddress = "u24641342@tuks.co.za" });
-            students.Add(new Models.StudentModel { _studentNumber = "u24704785", _name = "Busisiwe", _surname = "Paul", _emailAddress = "u24704785@tuks.co.za" });
-            students.Add(new Models.StudentModel { _studentNumber = "u24687945", _name = "Naledi", _surname = "Nkwana", _emailAddress = "u24687945@tuks.co.za" });
-            students.Add(new Models.StudentModel { _studentNumber = "u22675524", _name = "Thato", _surname = "Modise", _emailAddress = "u22675524@tuks.co.za" });
+            students.Add(new Models.StudentModel { _studentNumber = "u24722490", _name = "Nyiko", _surname = "Mbhalati", _emailAddress = "u24722490@tuks.co.za", myLink = "~/HTML/HtmlPageNyiko.html"});
+            students.Add(new Models.StudentModel { _studentNumber = "u24641342", _name = "Michael", _surname = "Neto", _emailAddress = "u24641342@tuks.co.za", myLink = "~/HTML/HtmlPageMichael.html" });
+            students.Add(new Models.StudentModel { _studentNumber = "u24704785", _name = "Busisiwe", _surname = "Paul", _emailAddress = "u24704785@tuks.co.za", myLink = "~/HTML/HtmlPageBusi.html" });
+            students.Add(new Models.StudentModel { _studentNumber = "u24687945", _name = "Naledi", _surname = "Nkwana", _emailAddress = "u24687945@tuks.co.za", myLink = "~/HTML/HtmlPageNaledi.html" });
+            students.Add(new Models.StudentModel { _studentNumber = "u22675524", _name = "Thato", _surname = "Modise", _emailAddress = "u22675524@tuks.co.za", myLink = "~/HTML/HtmlPageThato.html" });
 
             return View(students);
         }
